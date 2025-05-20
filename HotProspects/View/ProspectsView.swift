7c3ba56700dc4b19dac090fc1ef9fb676e85d57c
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-20 13:28:19 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/posting-notifications-to-the-lock-screen

--- HotProspects/View/ProspectsView.swift
+++ HotProspects/View/ProspectsView.swift
@@ -6,6 +6,7 @@
 import SwiftUI
 import SwiftData
 import CodeScanner
+import UserNotifications
 
 struct ProspectsView: View {
     
@@ -62,6 +63,10 @@ struct ProspectsView: View {
                             prospect.isContacted.toggle()
                         }
                         .tint(.blue)
+                        Button("Remind Me", systemImage: "bell") {
+                            addNotification(for: prospect)
+                        }
+                        .tint(.orange)
                     } else {
                         Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
                             prospect.isContacted.toggle()
@@ -112,6 +117,39 @@ struct ProspectsView: View {
     func delete() {
         for prospect in selectedProspects {
             modelContext.delete(prospect)
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
         }
     }
     
